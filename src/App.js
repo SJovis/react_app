6eb8927be5f77f3fef605f133da0c1053f2de77a
Sérgio Assis Cerqueira <jovis.sergio@gmail.com>
2022-04-18 15:14:58 +0100

    new expenses added to the list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import { useState } from 'react';
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const App = () => {
-
-  const expenses = [
+const DUMMY_EXPENSES = [
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -25,8 +25,14 @@ const App = () => {
     },
   ];
 
+const App = () => {
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
   const addExpenseHandler = (expense) => {
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
